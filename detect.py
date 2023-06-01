12e9ef05a609ec74d3376456ed36982ec1b676ea
Pawilon99 <123984069+Pawilon99@users.noreply.github.com>
2023-06-02 00:13:28 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,18 +2,18 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 
-# Set ArUco marker parameters
+# Dekalaracja parametrów ArUco
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 aruco_params = aruco.DetectorParameters_create()
 
-# Set camera parameters
+# Deklaracja parametrów kamery
 camera_matrix = np.genfromtxt('cameraMatrix.txt', dtype=float, encoding=None, delimiter=',')
 camera_distortion = np.genfromtxt('cameraDistortion.txt', dtype=float, encoding=None, delimiter=',')
 
 
 
-# Capture video from camera
-cap = cv2.VideoCapture(0)  # Use camera index 0, change to the appropriate index if using multiple cameras
+# Zainicjalizowanie działania kamery
+cap = cv2.VideoCapture(0) 
 
 if __name__ == '__main__':
     rospy.init_node('publisher', anonymous=True)
@@ -22,46 +22,45 @@ if __name__ == '__main__':
     list_param = []
 
     while True:
-        # Capture frame from camera
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Convert frame to grayscale
+        # Przekształcenie obrazu w czarno-biały
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect ArUco markers
+        # Wychwycenie znaczników
         corners, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
  
         if ids is not None:
-            # Estimate marker poses
+            # Oszacowanie pozycji markerów
             rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, camera_matrix, camera_distortion)
 
-            # Draw marker poses on frame
+            # Wyrysowanie pozcyji markerów na obrazie
             for i in range(len(ids)):
 
                 distance = np.sqrt(tvecs[i][0][2]**2 + tvecs[i][0][0]**2 + tvecs[i][0][1]**2
                 cv2.drawFrameAxes(frame, camera_matrix, camera_distortion, rvecs[i], tvecs[i], 0.01)
                 aruco.drawDetectedMarkers(frame, corners)
-                cv2.putText(
-                frame, 
-                f"id: {ids} Dist: {round(distance*100,2)}",
-                (10,50),
-                cv2.FONT_HERSHEY_PLAIN,
-                1, 
-                (200,100,255), 
-                2, 
-                cv2.LINE_AA)
+#                 cv2.putText(
+#                 frame, 
+#                 f"id: {ids} Dist: {round(distance*100,2)}",
+#                 (10,50),
+#                 cv2.FONT_HERSHEY_PLAIN,
+#                 1, 
+#                 (200,100,255), 
+#                 2, 
+#                 cv2.LINE_AA)
 
-                cv2.putText(
-                frame, 
-                f"id: {ids} x: {round((tvecs[i][0][0])*100,1)} y: {round((tvecs[i][0][1])*100,1)}",
-                (10,100),
-                cv2.FONT_HERSHEY_PLAIN,
-                1, 
-                (200,100,255), 
-                2, 
-                cv2.LINE_AA)
+#                 cv2.putText(
+#                 frame, 
+#                 f"id: {ids} x: {round((tvecs[i][0][0])*100,1)} y: {round((tvecs[i][0][1])*100,1)}",
+#                 (10,100),
+#                 cv2.FONT_HERSHEY_PLAIN,
+#                 1, 
+#                 (200,100,255), 
+#                 2, 
+#                 cv2.LINE_AA)
 
 
             # Print marker IDs and poses
