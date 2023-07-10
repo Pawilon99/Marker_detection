a4026a349eeea0e8630fb5ea55d4f6f223b4110a
Pawilon99 <123984069+Pawilon99@users.noreply.github.com>
2023-07-10 13:51:08 +0200

    Update obliczenia.py

--- obliczenia.py
+++ obliczenia.py
@@ -2,18 +2,24 @@ import rospy
 import numpy as np
 import cv2
 from my_project.msg import vectors
+import json
 
 tvecs_rec = 0
 rvecs_rec = 0
 ids = 0
 raspberry = 0
 distance = 0
-rvecs = 0
-tvecs = 0
 info1 = False
+info2 = False
 rotation_matrix = 0
 point = 0
-info2 = False
+coordinates = 0
+T1 = 0
+T1_inv = 0
+T2 = 0
+T2_inv = 0
+vector = 0
+j = {}
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
@@ -21,53 +27,61 @@ def callback(data):
     global rvecs_rec
     global ids
     global raspberry
-    global distance
-    global tvecs
-    global rvecs
-    global info
+    global info1
+    global info2
     global rotation_matrix
     global point
-
-    tvecs_rec = [data.x, data.y, data.z]
-    rvecs_rec = [data.i, data.j, data.k]
-    tvecs = np.array(tvecs_rec)
-    rvecs = np.array(rvecs_rec)
-    #ids = [data.l]
-    raspberry = [data.m]
+    global coordinates
+    global T1
+    global T1_inv
+    global T2
+    global T2_inv
+    global vector
+    global j
     
-    if info1==False and raspberry == 1.0:
-        rotation_matrix = cv2.Rodrigues(rvecs)
-        distance = np.sqrt(tvecs[0]**2 + tvecs[1]**2 + tvecs[2]**2)
-        T = np.concatenate((rotation_matrix, tvecs), axis=1)
-        T = np.concatenate((T, np.array([[0, 0, 0, 1]])), axis=0)
-        T_inv = np.linalg.inv(T)
-        point_camera = np.array([[0],[0], [distance]])
-        new_point = np.vstack((point_camera, 1.0))
-        point_desire = T_inv.dot(new_point)
-        point = point_desire[:3]
+    tvecs_rec = np.array([[data.x], [data.y], [data.z]])
+    rvecs_rec = np.array([[data.i], [data.j], [data.k]])
+    raspberry = data.m
+    
+    if raspberry == 1.0 and info1 == False:
+        rotation_matrix, _ = cv2.Rodrigues(rvecs_rec)
+        T1 = np.concatenate((rotation_matrix, tvecs_rec), axis=1)
+        T1 = np.concatenate((T1, np.array([[0, 0, 0, 1]])), axis=0)
+        T1_inv = np.linalg.inv(T1)
+    
+        info1 = True
 
-        info = True
+    if raspberry == 2.0 and info2 == False:
+        rotation_matrix, _ = cv2.Rodrigues(rvecs_rec)
+        T2 = np.concatenate((rotation_matrix, tvecs_rec), axis=1)
+        T2 = np.concatenate((T2, np.array([[0, 0, 0, 1]])), axis=0)
+        T2_inv = np.linalg.inv(T2)
+
+        info2 = True
+
+    if (raspberry == 1.0):
+        vector = np.array([[0],[0],[0],[1]])
+        rotation_matrix, _ = cv2.Rodrigues(rvecs_rec)
+        T1 = np.concatenate((rotation_matrix, tvecs_rec), axis=1)
+        T1 = np.concatenate((T1, np.array([[0, 0, 0, 1]])), axis=0)
+        point = T1_inv@T1@vector
+        j['coordinates'] = point.tolist()
+        coordinates = point 
+    else:
+        vector = np.array([[0],[0],[0],[1]])
+        rotation_matrix, _ = cv2.Rodrigues(rvecs_rec)
+        T2 = np.concatenate((rotation_matrix, tvecs_rec), axis=1)
+        T2 = np.concatenate((T2, np.array([[0, 0, 0, 1]])), axis=0)
+        point = T2_inv@T2@vector
+        j['coordinates1'] = point.tolist()
+        coordinates = point
         
-    if info2==False and raspberry == 2.0:
-        rotation_matrix = cv2.Rodrigues(rvecs)
-        distance = np.sqrt(tvecs[0]**2 + tvecs[1]**2 + tvecs[2]**2)
-        T = np.concatenate((rotation_matrix, tvecs), axis=0)
-        T = np.concatenate((T, np.array([[0, 0, 0, 1]])), axis=0)
-        T_inv = np.linalg.inv(T)
-        point_camera = np.array([[0],[0], [distance]])
-        new_point = np.vstack((point_camera, 1.0))
-        point_desire = T_inv.dot(new_point)
-        point = point_desire[:3]
-
-        info = True
+    print(coordinates)  
     
-    # if (raspberry == 1.0):
-    #     distance = np.sqrt(tvecs[0]**2 + tvecs[1]**2 + tvecs[2]**2)
-    # else:
-    #     distance = np.sqrt(tvecs[0]**2 + tvecs[1]**2 + tvecs[2]**2)
-
-    print(point)
     
+    with open('/home/asus/catkin_ws/src/my_project/scripts/coordinates.json', 'w') as jsonFile:
+        json.dump(j,jsonFile)
+        jsonFile.close()
 
 def listener():
 
